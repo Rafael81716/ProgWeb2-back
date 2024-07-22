5a0f9070a30d1240ffe04cb9b7d6affa5400575a
Rafael81716 <rafacavalcante081@gmail.com>
2024-07-22 08:48:50 -0300

    feat: all api routes are working

--- src/server.ts
+++ src/server.ts
@@ -1,60 +1,116 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, request } from 'express';
 import path from 'path';
 
 
 const app = express();
 const port = 3000;
-const characters = [
+let characters = [
   {
     id: 1,
     name: 'John Doe',
-    class: 'Fighter',
-    Level: 1,
+    classType: 'Fighter',
+    level: 1,
     HP: 20
   }, {
     id: 2,
     name: 'Mariano Doe',
-    class: 'Paladin',
-    Level: 5,
+    classType: 'Paladin',
+    level: 5,
     HP: 50
   },
   {
     id: 3,
     name: 'Jonas Doe',
-    class: 'wizard',
-    Level: 10,
+    classType: 'wizard',
+    level: 10,
     HP: 90
   }
 ]
+let currentId = 4;
 
 app.use(express.json());
 
-app.get('/index', (req, res) => {
+app.get('/index', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '..', 'public/index.html'));
 })
 
 app.get('/characters', (req: Request, res: Response) => {
-  res.send(characters);
+  res.json(characters);
 });
 
-app.get('/characters/1', (req: Request, res: Response) => {
-  res.send(characters.filter((char) => char.id == 1));
+app.get('/characters/:id', (req: Request, res: Response) => {
+  const char = characters.filter((char) => char.id == parseInt(req.params.id));
+  if (char) {
+    res.json(char);
+  } else {
+    res.status(404).send('Item not found');
+  }
 });
 
 app.post('/characters', (req: Request, res: Response) => {
-  res.send('Requisição Post a homePage');
+  const {name, classType, level, HP } = req.body;
+  if (!name) {
+    return res.status(400).send('Name is required');
+  }
+  if (!classType) {
+    return res.status(400).send('classType is required');
+  }
+  if (!level) {
+    return res.status(400).send('Level is required');
+  }
+  if (!HP) {
+    return res.status(400).send('HP is required');
+  }
+  const char = { id: currentId++, name, classType, level, HP};
+  characters.push(char);
+  res.status(201).json(char);
 });
 
-app.put('/characters/1', (req: Request, res: Response) => {
-  res.send('Requisição Post a homePage');
+app.put('/characters/:id', (req: Request, res: Response) => {
+  const index = characters.findIndex(i => i.id === parseInt(req.params.id));
+    if (index !== -1) {
+      const {name, classType, level, HP } = req.body;
+    if (!name) {
+      return res.status(400).send('Name is required');
+    }
+    if (!classType) {
+      return res.status(400).send('classType is required');
+    }
+    if (!level) {
+      return res.status(400).send('Level is required');
+    }
+    if (!HP) {
+      return res.status(400).send('HP is required');
+    }
+    characters[index] = { id: index, name, classType, level, HP};
+    res.json(characters[index]);
+  } else {
+    res.status(404).send('Character not found');
+  }
 });
 
-app.delete('/characters/1', (req: Request, res: Response) => {
-  res.send('Requisição Post a homePage');
+app.delete('/characters/:id', (req: Request, res: Response) => {
+  const index = characters.findIndex(i => i.id === parseInt(req.params.id));
+  if (index !== -1) {
+    characters.splice(index, 1);
+    res.status(204).send();
+  } else {
+    res.status(404).send('Character not found');
+  }
 });
 
-app.patch('/characters/1/HitPoints', (req: Request, res: Response) => {
-  res.send('Requisição Post a homePage');
+app.patch('/characters/:id/HitPoints', (req: Request, res: Response) => {
+  const index = characters.findIndex(i => i.id === parseInt(req.params.id));
+  if (index !== -1) {
+    const { HP } = req.body;
+    if (!HP) {
+      return res.status(400).send('HitPoints is required');
+    }
+    characters[index] = { ...characters[index], HP };
+    res.json(characters[index]);
+  } else {
+    res.status(404).send('Character not found');
+  }
 });
 
 app.listen(port, () => {
