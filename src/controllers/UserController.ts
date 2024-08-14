20596d2a63ac1170666b27707d9d117ca3ca364a
Rafael <rafacavalcante081@gmail.com>
2024-08-14 20:36:04 -0300

    feat: validation for user are done

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,13 +1,19 @@
 import { Request, Response } from 'express'
 import { UserService } from '../services/UserService';
+import { userSchema } from '../validation/UserSchema';
 
 export class UserController {
     async createUser(request: Request, response: Response){
         try{
-            const userService = new UserService();
-            const userCreated = await userService.createUser(request.body)
-            console.log(userCreated)
-            return response.status(200).json(userCreated);
+            const validated = userSchema.validate(request.body);
+            if(!validated.error){
+                const userService = new UserService();
+                const userCreated = await userService.createUser(request.body)
+                console.log(userCreated)
+                return response.status(200).json(userCreated);
+            }else{
+                return response.status(400).json({ error: "Input Error" })
+            }
         }catch(error){
             console.error('Error creating User: ', error);
             return response.status(500).json({ error: "An error has ocurred while fetching the user." });
@@ -56,12 +62,17 @@ export class UserController {
     async updateUser(request: Request, response: Response){
         const { id } = request.params;
         try{
-            const userService = new UserService();
-            const updatedUser = await userService.updateUser(request.body, Number(id))
-            if(!updatedUser){
-                return response.status(404).json({ error: "User not found." })
+            const validated = userSchema.validate(request.body);
+            if(!validated.error){
+                const userService = new UserService();
+                const updatedUser = await userService.updateUser(request.body, Number(id))
+                if(!updatedUser){
+                    return response.status(404).json({ error: "User not found." })
+                }else{
+                    return response.status(200).json(updatedUser) 
+                }
             }else{
-                return response.status(200).json(updatedUser) 
+                return response.status(400).json({ error: "Input Error" })
             }
         }catch(error){
             console.error('Error creating User: ', error);
@@ -71,12 +82,17 @@ export class UserController {
     async patchUser(request: Request, response: Response){
         const { id } = request.params;
         try{
-            const userService = new UserService();
-            const updatedUser =  await userService.patchUser(request.body, Number(id))
-            if(!updatedUser){
-                return response.status(404).json({ error: "User not found." })
+            const validated = userSchema.validate(request.body);
+            if(!validated.error){
+                const userService = new UserService();
+                const updatedUser =  await userService.patchUser(request.body, Number(id))
+                if(!updatedUser){
+                    return response.status(404).json({ error: "User not found." })
+                }else{
+                    return response.status(200).json(updatedUser) 
+                }
             }else{
-                return response.status(200).json(updatedUser) 
+                return response.status(400).json({ error: "Input Error" })
             }
         }catch(error){
             console.error('Error creating User: ', error);
